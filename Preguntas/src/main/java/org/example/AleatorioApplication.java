3ca8511f97da3ce608f4871475d0c8749ed556c0
LARA <92118085+LARA972010@users.noreply.github.com>
2025-02-18 20:09:55 +0100

    Add files via upload

---
+++ Preguntas/src/main/java/org/example/AleatorioApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleatorioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AleatorioApplication.class, args);
+    }
+}
